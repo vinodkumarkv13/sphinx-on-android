1f919c31699049fcc7916435ef4f65dae30e6997
gqwmail@gmail.com <gqwmail@gmail.com@bfd3f6e4-e0fe-0b14-43e3-1a44d9b9b67c>
2011-09-20 03:55:04 +0000

    change project name

--- cmu_test_remote/android_test/cp_jni.sh
+++ cmu_test_remote/android_test/cp_jni.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-cd /home/q/Workspace/sphinx-on-android/trunk/cmu_test_remote/android_test/
+cd /home/q/Workspace/sphinx-on-android/trunk/cmu_test_remote/AndroidSphinx/
 javah -d /home/q/Workspace/sphinx-on-android/trunk/cmu_test_remote/android_exe/include/ -classpath ./bin/ -jni android.gqweb.net.jni.SphinxJniCall
